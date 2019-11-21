10d290770b0b154772c9b9a75fc1f5671b7bd5bf
Admin <conner.p.mccabe@gmail.com>
2019-11-21 15:11:14 -0600

    file structured

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Weather APP</h1>
-    </div>
-  );
-}
+class App extends React.Component {
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Weather APP</h1>
+      </div>
+    );
+  } 
+   
+} 
+  
+
 
 export default App;
