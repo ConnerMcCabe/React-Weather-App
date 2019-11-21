78a7accfef0b60b623c2da206c3ab3b657c517bd
Admin <conner.p.mccabe@gmail.com>
2019-11-21 15:34:49 -0600

    weather comp

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Titles from "../component/Titles";
 import Form from "../component/Form";
+import Weather from "../component/Weather";
 import './App.css';
 
 class App extends React.Component {
@@ -11,6 +12,7 @@ class App extends React.Component {
       <div className="App">
         <Titles />
         <Form />
+        <Weather />
       </div>
     );
   } 
