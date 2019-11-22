cb51d22225cc0beefc95babdcdd0eff9fa7329d3
Admin <conner.p.mccabe@gmail.com>
2019-11-22 16:21:15 -0600

    getWeather async started

--- src/App/App.js
+++ src/App/App.js
@@ -6,10 +6,14 @@ import './App.css';
 
 const API_KEY = "  ";
 
+
+
 class App extends React.Component {
 
   getWeather = async (e) => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json(); 
+    console.log(data);
   }
 
   render() {
--- src/component/Form.js
+++ src/component/Form.js
@@ -3,9 +3,10 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <div>
-        future form comp
-      </div>
+      <form>
+        <input type="text" name="city" placeholder="City..."/>
+        <input type="text" name="country" placeholder="Country..."/>
+      </form>
     )
   }
 }
