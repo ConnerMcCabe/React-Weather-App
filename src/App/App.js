2562ce6acd753eb2a616219bc948734fd60ffb84
Conner McCabe <conner.p.mccabe@gmail.com>
2020-06-16 15:26:57 -0500

    rewriting getWeather

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Titles from "../component/Titles";
 import Form from "../component/Form";
 import Weather from "../component/Weather";
+import { getWeather } from "../api/fetchWeather"
 import './App.css';
 
 
-class App extends React.Component {
+const App = () => {
+  const [query, setQuery] = useState('');
 
-  render() {
+  const search = async(e) => {
+    if(e.key === 'Enter') {
+      const data = await getWeather(query)
+      console.log(data)
+    }
+  }
     return (
-      <div className="App">
+      <div className="main-container">
         <Titles />
+        <input 
+          type="text"
+          className="search"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        
         <Form />
         <Weather />
       </div>
     );
-  } 
-} 
+  }
   
 
 
