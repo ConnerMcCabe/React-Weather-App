4374d3a0c6a01b896362dfe3367927fb2883fc25
Conner McCabe <conner.p.mccabe@gmail.com>
2020-06-17 11:15:01 -0500

    added weather icon

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'http://api.openweathermap.org/data/2.5/weather';
-const API_KEY = '';
+const API_KEY = '788225eafda5da881be33adf45e54af4';
 
 export const getWeather = async (query) => {
     const { data } = await axios.get(URL, {
